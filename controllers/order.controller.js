f16776ad58f908fb851342fd7072f6b6b827ff68
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-09-15 21:16:33 +0400

    bugfix(cors):test cors module

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -7,8 +7,6 @@ exports.create = (req, res, next) => {
 
     const orderObject = req.body;
     console.log('orderObject:', orderObject);
-    // const fullName = orderObject.customer.firstName + " " + orderObject.customer.lastName
-    // console.log(orderObject)
     const order = new Order({
         ...orderObject
     });
@@ -19,48 +17,16 @@ exports.create = (req, res, next) => {
 
 exports.getPaymentSecret = (req, res, next) => {
     const orderObject = req.body;
-    // console.log(orderObject)
-    // const fullName = orderObject.customer.firstName + " " + orderObject.customer.lastName
-    // console.log(orderObject.customer.email)
-    // const order = new Order({
-    //     ...orderObject
-    // });
-    //charge customer 
-
-
     stripe.paymentIntents.create({
         amount: orderObject.amount * 100,
         currency: 'eur',
         payment_method_types: ["card"],
-        // confirm: true,
-        // // payment_method: '{{PAYMENT_METHOD_ID}}',
-        // payment_method_options: {
-        //     card: {
-        //         network: 'cartes_bancaires',
-        //     },
-        // },
-        // metadata: { 'Client': fullName },
-        // receipt_email: orderObject.customer.email
+
     }).then((response) => {
         // console.log('intent', response)
         res.status(200).json({ client_secret: response.client_secret })
     }).catch((error) => { console.log('error', error) })
 
-    // const intent = await stripe.paymentIntents.create({
-    //     amount: orderObject.amount * 100,
-    //     currency: 'eur',
-    //     payment_method_types: ["card"],
-    //     // confirm: true,
-    //     // // payment_method: '{{PAYMENT_METHOD_ID}}',
-    //     // payment_method_options: {
-    //     //     card: {
-    //     //         network: 'cartes_bancaires',
-    //     //     },
-    //     // },
-    //     // metadata: { 'Client': fullName },
-    //     // receipt_email: orderObject.customer.email
-    // });
-    // console.log("intent", intent);
-    // res.json({ client_secret: intent.client_secret });
+
 
 }
