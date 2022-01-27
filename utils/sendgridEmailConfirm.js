f811fd99c99adcfbfb2f9659d0ba604f82d430b7
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-01-27 10:27:06 +0400

    Fix: text error

--- utils/sendgridEmailConfirm.js
+++ utils/sendgridEmailConfirm.js
@@ -252,7 +252,7 @@ const sendGridEmail = (confirmation) => {
                     <tr>
                         <td align="left" bgcolor="#ffffff"
                             style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-                            <p style="margin: 0;">A bienôt,<br> Team Exo-trap</p>
+                            <p style="margin: 0;">Pullement votre,<br> La team Exo-trap</p>
                         </td>
                     </tr>
                     <!-- end copy -->
