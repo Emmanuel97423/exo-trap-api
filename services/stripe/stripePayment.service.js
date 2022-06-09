f97c70238b70cfe78bacac6d3fdd375efab89d3a
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-06-09 09:14:14 +0400

    Feature(payment): Init clear pay

--- services/stripe/stripePayment.service.js
+++ services/stripe/stripePayment.service.js
@@ -9,18 +9,18 @@ const createCheckoutStripePayment = async (req, res, next) => {
 
     try {
         const products = req.body;
-        const productsArrayStripeModel = [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'T-shirt',
-                    },
-                    unit_amount: 2000,
-                },
-                quantity: 1,
-            },
-        ];
+        // const productsArrayStripeModel = [
+        //     {
+        //         price_data: {
+        //             currency: 'usd',
+        //             product_data: {
+        //                 name: 'T-shirt',
+        //             },
+        //             unit_amount: 2000,
+        //         },
+        //         quantity: 1,
+        //     },
+        // ];
         const productsArray = []
 
         products.forEach(product => {
@@ -29,9 +29,26 @@ const createCheckoutStripePayment = async (req, res, next) => {
 
         });
         const session = await stripe.checkout.sessions.create({
-
+            payment_method_types: ['card', 'afterpay_clearpay'],
             line_items: productsArray,
             mode: 'payment',
+            shipping_address_collection: {
+                // Specify which shipping countries Checkout should provide as options for shipping locations
+                allowed_countries: ['FR'],
+            },
+            // If you already have the shipping address, provide it in payment_intent_data:
+            payment_intent_data: {
+                shipping: {
+                    name: 'Jenny Rosen',
+                    address: {
+                        line1: '1234 Main Street',
+                        city: 'San Francisco',
+                        state: 'CA',
+                        country: 'US',
+                        postal_code: '94111',
+                    },
+                },
+            },
             success_url: process.env.CLIENT_URL + '/payment/sucess?session_id={CHECKOUT_SESSION_ID}',
             cancel_url: process.env.CLIENT_URL + '/',
         });
