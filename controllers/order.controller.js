062f79415bb8b3eaafb3b483fb358323c1df6142
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-08-04 17:47:40 +0400

    Hotfix(status order): Update pending status

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -23,7 +23,7 @@ exports.create = (req, res, next) => {
         ...orderObject,
         date: dayjs().locale('fr').format('dddd, D MMMM, YYYY HH:mm'),
         orderNumberId: dayjs().locale('fr').format('MMDDYYHHmm'),
-        status: "En cours de traitement..."
+        status: "En attente..."
     });
     order.save().then((order) => {
         console.log("🚀 ~ file: order.controller.js ~ line 48 ~ order.save ~ order", order)
