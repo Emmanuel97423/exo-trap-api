1b6612a7cb984fa0454bafe28962f6c5675a2a86
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-29 18:06:57 +0400

    Feature(search): search by tags subcategory ok no refactoring do

--- routes/search.route.js
+++ routes/search.route.js
@@ -44,40 +44,237 @@ router.get('/filter', async (req, res, next) => {
 });
 
 router.get('/filter/subCategory', async (req, res, next) => {
-    const productsArray = [];
-
+    const productsArrayTemp = [];
+    const arrayRequest = req.query.searchArrayByTags
+    console.log('arrayRequest:', arrayRequest)
     const searchProductBySubCategoryRequest = req.query.search;
-    ProductGamme.find({ codeSousFamille: searchProductBySubCategoryRequest }, (error, products) => {
-        if (error) res.status(500).json({ error: error });
-        if (products) {
-            console.log('products:', products)
-            products.filter(product => {
-                productsArray.push(product);
 
+    if (!arrayRequest) {
+        return
+    } {
+        const myPromise = new Promise((resolve, reject) => {
+            let indexTemp = arrayRequest.length
+            arrayRequest.map((request) => {
+
+                ProductGamme.find({ libelleSousFamille: request }, (error, products) => {
+                    if (error) res.status(500).json({ error: error });
+                    if (products) {
+                        // console.log('products:', products)
+                        products.filter(product => {
+                            productsArrayTemp.push(product);
+
+                        });
+                        Product.find({ libelleSousFamille: request }, (error, singleProduct) => {
+                            if (error) res.status(500).json({ error: error });
+                            if (singleProduct) {
+                                console.log('singleProduct:', singleProduct)
+                                singleProduct.filter(product => {
+                                    if (product.codeGamme === '') {
+
+                                        productsArrayTemp.push(product);
+
+
+
+
+                                    }
+
+                                })
+                                // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
+
+
+
+
+                            };
+                            console.log('indexTemp:', indexTemp)
+                            indexTemp--
+                            if (indexTemp == 0) {
+                                console.log('indexTemp:', indexTemp)
+
+                                setTimeout(() => {
+                                    resolve(productsArrayTemp);
+                                }, 200)
+                            }
+
+                        })
+
+                    };
+
+
+                });
             });
-            Product.find({ codeSousFamille: searchProductBySubCategoryRequest }, (error, singleProduct) => {
-                if (error) res.status(500).json({ error: error });
-                if (singleProduct) {
-                    singleProduct.filter(product => {
-                        if (product.codeGamme === '') {
-
-                            productsArray.push(product);
-
-                        }
-
-                    })
-                    // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
-
-                    return res.status(200).json({ productsArray })
-
-                }
-
-            })
-
-        };
 
 
-    });
+            // for (let i = 0; i < arrayRequest.length; i++) {
+
+            //     ProductGamme.find({ libelleSousFamille: arrayRequest[i] }, (error, products) => {
+            //         if (error) res.status(500).json({ error: error });
+            //         if (products) {
+            //             products.filter(product => {
+            //                 productsArrayTemp.push(product);
+
+            //             });
+            //             Product.find({ libelleSousFamille: arrayRequest[i] }, (error, singleProduct) => {
+            //                 if (error) res.status(500).json({ error: error });
+            //                 if (singleProduct) {
+            //                     singleProduct.filter(product => {
+            //                         if (product.codeGamme === '') {
+            //                             console.log('arrayRequest.length - i:', i);
+            //                             console.log('arrayRequest.length:', arrayRequest.length - i)
+            //                             productsArrayTemp.push(product);
+            //                             // resolve('Promise OK');
+            //                             if (i == 1) {
+
+            //                                 resolve(productsArrayTemp);
+            //                             } else {
+            //                                 reject(null)
+            //                             }
+
+            //                         }
+
+            //                     })
+            //                     // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
+
+
+
+
+            //                 }
+
+            //             })
+
+            //         };
+
+
+            //     });
+            //     // console.log('arrayRequest.length:', arrayRequest.length - i)
+
+            //     // console.log('i:', i);
+
+
+            // }
+
+            // return
+            // arrayRequest.map(libelleSousFamille => {
+            //     ProductGamme.find({ libelleSousFamille: libelleSousFamille }, (error, products) => {
+            //         if (error) res.status(500).json({ error: error });
+            //         if (products) {
+            //             products.filter(product => {
+            //                 productsArray.push(product);
+
+            //             });
+            //             Product.find({ libelleSousFamille: libelleSousFamille }, (error, singleProduct) => {
+            //                 if (error) res.status(500).json({ error: error });
+            //                 if (singleProduct) {
+            //                     singleProduct.filter(product => {
+            //                         if (product.codeGamme === '') {
+
+            //                             productsArray.push(product);
+            //                             resolve('Promise OK')
+
+            //                         }
+
+            //                     })
+            //                     // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
+
+
+
+
+            //                 }
+
+            //             })
+
+            //         };
+
+
+            //     });
+
+            // });
+
+
+        })
+        myPromise.then((productsArray) => {
+            // console.log('result:', result)
+            // console.log('res:', res)
+            console.log('productsArray:', productsArray)
+
+            return res.status(200).json({ productsArray })
+
+
+        }).catch((err) => { console.log('err:', err) })
+        // arrayRequest.map(libelleSousFamille => {
+        //     ProductGamme.find({ libelleSousFamille: libelleSousFamille }, (error, products) => {
+        //         if (error) res.status(500).json({ error: error });
+        //         if (products) {
+        //             console.log('products:', products)
+        //             products.filter(product => {
+        //                 productsArray.push(product);
+
+        //             });
+        //             Product.find({ libelleSousFamille: libelleSousFamille }, (error, singleProduct) => {
+        //                 if (error) res.status(500).json({ error: error });
+        //                 if (singleProduct) {
+        //                     console.log('singleProduct:', singleProduct)
+        //                     singleProduct.filter(product => {
+        //                         if (product.codeGamme === '') {
+
+        //                             productsArray.push(product);
+
+        //                         }
+
+        //                     })
+        //                     // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
+        //                     return productsArray
+
+
+
+        //                 }
+
+        //             })
+
+        //         };
+
+
+        //     });
+
+        // });
+
+    }
+
+    // for (let i = 0; i < arrayRequest.length; i++) {
+    //     console.log('arrayRequest:', arrayRequest[i])
+
+    // }
+
+
+    // ProductGamme.find({ codeSousFamille: searchProductBySubCategoryRequest }, (error, products) => {
+    //     if (error) res.status(500).json({ error: error });
+    //     if (products) {
+    //         products.filter(product => {
+    //             productsArray.push(product);
+
+    //         });
+    //         Product.find({ codeSousFamille: searchProductBySubCategoryRequest }, (error, singleProduct) => {
+    //             if (error) res.status(500).json({ error: error });
+    //             if (singleProduct) {
+    //                 singleProduct.filter(product => {
+    //                     if (product.codeGamme === '') {
+
+    //                         productsArray.push(product);
+
+    //                     }
+
+    //                 })
+    //                 // console.log("🚀 ~ file: search.route.js ~ line 19 ~ Product.find ~ productsArray", productsArray)
+
+    //                 return res.status(200).json({ productsArray })
+
+    //             }
+
+    //         })
+
+    //     };
+
+
+    // });
 
 
 });
