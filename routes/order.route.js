049a98a5b9c66c43dbab892702f66e6a95426581
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-11-02 10:49:24 +0400

    ETE-62 condition status Ok by color

--- routes/order.route.js
+++ routes/order.route.js
@@ -6,6 +6,7 @@ router.post("/addOrder", orderCtrl.create)
 router.post('/paymentSecret', orderCtrl.getPaymentSecret)
 router.get("/allOrders/:id", orderCtrl.allOrders)
 router.get("/allOrdersAdmin", orderCtrl.allOrdersAdmin)
+router.post("/changeOrderStatus", orderCtrl.changeOrderStatus)
 router.get("/:id", orderCtrl.getOne)
 router.post("/create-checkout-session", orderCtrl.stripeCheckout)
 
