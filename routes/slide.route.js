eb609dcfdf93bb09b66a1d7eae9f910623d5cbbb
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-12-14 11:19:50 +0400

    Add and update slide landing page 01

--- routes/slide.route.js
+++ routes/slide.route.js
@@ -3,13 +3,31 @@ const router = express.Router();
 const Slide = require('../models/Slide.model');
 const multer = require('multer');
 const DIR = './images/';
+// Require the Cloudinary library
+const cloudinary = require('cloudinary').v2;
+//Cloudinary access config
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+// cloudinary.api.delete_upload_preset("my_preset").then(result=>console.log('delete preset', result));
+cloudinary.api.create_upload_preset(
+  { name: "my_preset", 
+    unsigned: true, 
+    public_id: "slide-landing-page01" ,
+    // categorization: "google_tagging,google_video_tagging",
+    // auto_tagging: 0.75,
+    // background_removal: "cloudinary_ai",  
+    folder: "slides" })
+  .then(result=>console.log(result));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, DIR)
     },
     filename: (req, file, cb) => {
-    console.log('file:', file)
+    // console.log('file:', file)
     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {     
          const name = file.fieldname.split(" ").join("_") + "."+ MIME_TYPES[file.mimetype];
       cb(null, name);
@@ -30,24 +48,41 @@ const MIME_TYPES = {
 };
 
 router.post('/', upload.single('slide01'), async (req, res) => {
-const url = req.protocol + '://' + req.get('host')
-         const extension = req.file.mimetype;
-const fileUrl = url +"/images/" + req.file.fieldname  + "." + MIME_TYPES[extension]
-console.log('fileUrl:', fileUrl)
+        const titleSlide = req.body.title;
+        const descriptionSlide = req.body.description;
+        const url = req.protocol + '://' + req.get('host')
+        const extension = req.file.mimetype;
+        const fileUrl = url +"/images/" + req.file.fieldname  + "." + MIME_TYPES[extension];
 
-// console.log('req:', req.file)
+        cloudinary.uploader.upload("./images/" + req.file.fieldname + "." + MIME_TYPES[extension] , {upload_preset: "my_preset"}, (error, result)=>{
+        if(error) console.log(error);
+        if(result) {
 
-const slide = new Slide({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: fileUrl,
-});
-// let err = slide.joiValidate(req.body);
-//   if (err) throw err;
-slide.save((err, result)=>{
-    if(err) return res.status(400).json({ "message": "Une Erreur s'est produite", "error": err });
-    if(result) return res.status(200).json({ "message": result})
-})
+        Slide.findOneAndUpdate({ imageUrl : result.secure_url } , { 
+          title:titleSlide,
+          description:descriptionSlide,
+          imageUrl : result.secure_url
+        }, { new :true }, (error, slideUpdateResult)=>{
+        if(error)  res.status(500).json({ "message": "Une Erreur s'est produite", "error": err });;
+        if(slideUpdateResult) res.status(200).json({"message": "Mise à jour terminé"});
+        if(!slideUpdateResult) {
+          const slide = new Slide({
+            slideCode:"slide01",
+            title: titleSlide,
+            description: descriptionSlide,
+            imageUrl: result.secure_url,
+        });
+        slide.save((err, result)=>{
+            if(err) return res.status(400).json({ "message": "Une Erreur s'est produite", "error": err });
+            if(result) return res.status(200).json({ "message": result})
+        })
+        }
+        })
+
+        }
+        });
+
+
 
 });
 
